2bc6c389de11035212c2bc1acf1716f1be342d13
Sigit <sigitprayogo392@gmail.com>
2025-10-08 11:31:33 +0700

    konfigurasi no-reply

--- backend_tryout/backend_tryout/settings.py
+++ backend_tryout/backend_tryout/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,11 +142,13 @@ DEFAULT_CHARSET = 'utf-8'
 FILE_CHARSET = 'utf-8'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "otp.tryoutsnbt@gmail.com"
-EMAIL_HOST_PASSWORD = "jial wqmx qzra nduq"
+EMAIL_HOST = "mail.pintuniv.com"
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True  # karena port 465 pakai SSL
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+
 
 LANGUAGE_CODE = 'en-us'
 
